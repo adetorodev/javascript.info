ff0b2523607400b78ee4ba6e5241e258e2779a7e
Ezekiel Adetoror <adetoroe787@gmail.com>
2023-02-04 08:51:35 +0100

    variables

---
+++ fundamentals/variable.js
@@ -0,0 +1,32 @@
+let message; // declare a variable
+message = 'Hello' // assign value to variable
+// mutiple variable declaration in line
+let user = "John", age = 24, geeting = "Hello"
+
+// mutiline
+let admin = "John"
+    adminAge = 34
+    greetAdmin = "Welcome"
+
+// The var keyword is almost the same as let . It also declares a variable, but in a slightly
+// different, “old-school” way
+
+let hello = "Hello World!"
+message = hello
+console.log(hello)
+console.log(message)
+
+/************************
+    Constants
+*********************** */ 
+
+const adminUser = "Admin"
+// adminUser = "Binbo" // TypeError
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+console.log(color)
