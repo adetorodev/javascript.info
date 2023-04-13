a456a8af9bc08248af1be4c4efec3193b2feb7ed
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-13 08:25:47 -0400

    function

---
+++ fundamentals/function.js
@@ -0,0 +1,29 @@
+// syntax
+// function showMessage() {
+//     console.log("Hello!")
+// }
+// showMessage();
+
+// function showMessage() {
+//   let message = "Hello, I'm JavaScript!"; // local variable
+//   console.log(message);
+// }
+// showMessage();
+
+// let userName = "John";
+// function showMessage() {
+//   let message = "Hello, " + userName;
+//   console.log(message);
+// }
+// showMessage(); // Hello, John
+
+// let userName = "John";
+// function showMessage() {
+//   userName = "Bob"; // (1) changed the outer variable
+//   let message = "Hello, " + userName;
+//   console.log(message);
+// }
+// console.log(userName); // John before the function call
+// showMessage();
+
+
