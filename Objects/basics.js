e6f347c11fc516385466297a2baba96f1c61ed6e
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-19 22:38:02 +0100

    objects

--- Objects/basics.js
+++ Objects/basics.js
@@ -1,2 +1,13 @@
 let userA = new Object(); // "object constructor" syntax
 let userB = {}; // "object literal" syntax
+
+
+// Literals and properties
+let user = {
+  name: "John",
+  age: 30,
+};
+
+// get fields of the object:
+console.log( user.name ); // John
+console.log( user.age ); // 30
