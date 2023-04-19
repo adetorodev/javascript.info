5ca9ce510da43f6931f57ed1020d582597886031
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-19 22:38:48 +0100

    objects

--- Objects/basics.js
+++ Objects/basics.js
@@ -3,15 +3,15 @@ let userB = {}; // "object literal" syntax
 
 
 // Literals and properties
-let user = {
-  name: "John",
-  age: 30,
-};
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
 
 // get fields of the object:
-console.log( user.name ); // John
-console.log( user.age ); // 30
+// console.log( user.name ); // John
+// console.log( user.age ); // 30
 
-user.isAdmin = true;
+// user.isAdmin = true;
 
-delete user.age;
+// delete user.age;
