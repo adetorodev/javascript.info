081c265c6124a302cc17220a803bfcb95d6fef6a
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-25 23:17:55 +0100

    exitense

--- Objects/basics.js
+++ Objects/basics.js
@@ -1,5 +1,5 @@
-let userA = new Object(); // "object constructor" syntax
-let userB = {}; // "object literal" syntax
+// let userA = new Object(); // "object constructor" syntax
+// let userB = {}; // "object literal" syntax
 
 // Literals and properties
 // let user = {
@@ -33,11 +33,11 @@ let userB = {}; // "object literal" syntax
 // // same as user["likes birds"] = true;
 // user[key] = true;
 
-let fruit = prompt("Which fruit to buy?", "apple");
-let bag = {
-  [fruit]: 5, // the name of the property is taken from the variable fruit
-};
-console.log(bag.apple); // 5 if fruit="apple"
+// let fruit = prompt("Which fruit to buy?", "apple");
+// let bag = {
+//   [fruit]: 5, // the name of the property is taken from the variable fruit
+// };
+// console.log(bag.apple); // 5 if fruit="apple"
 
 // let obj = {
 //   for: 1,
@@ -51,12 +51,32 @@ console.log(bag.apple); // 5 if fruit="apple"
 // obj.__proto__ = 5;
 // console.log(obj.__proto__); // [object Object], didn't work as intended
 
-function makeUser(name, age) {
-  return {
-    name: name,
-    age: age,
-    // ...other properties
-  };
+// function makeUser(name, age) {
+//   return {
+//     name: name,
+//     age: age,
+//     // ...other properties
+//   };
+// }
+// let user = makeUser("John", 30);
+// console.log(user.name); // John
+
+
+// let user = {}
+// console.log(user.noSuchProperty === undefined);
+
+// let user = {
+//   name: "John",
+//   age: 30
+// }
+
+// console.log("age" in user);
+// console.log("blabla" in user);
+// console.log("John" in user);
+
+let obj = {
+  test: undefined
 }
-let user = makeUser("John", 30);
-console.log(user.name); // John
+
+console.log(obj.test);
+console.log("test" in obj);
