04a768144760ed4526eff268e91005008ed8c0f8
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-27 23:15:20 +0100

    gabbage collection 2

--- Objects/gabbageCollection.js
+++ Objects/gabbageCollection.js
@@ -4,12 +4,34 @@
 
 // user = null
 
-let user = {
-  name: "John",
-};
+// let user = {
+//   name: "John",
+// };
 
-let admin = user;
-user= null
+// let admin = user;
+// user= null
 
-console.log(user); // user is null
-console.log(admin); // admin still remain in memory
+// console.log(user); // user is null
+// console.log(admin); // admin still remain in memory
+
+function marry(man, woman) {
+  woman.husband = man;
+  man.wife = woman;
+  return {
+    father: man,
+    mother: woman,
+  };
+}
+let family = marry(
+  {
+    name: "John",
+  },
+  {
+    name: "Ann",
+  }
+);
+
+console.log(family);
+
+delete family.father
+console.log(family);
