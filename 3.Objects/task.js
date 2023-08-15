e91ce0f8da9865bf763e69d849bd304949a8c06e
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-15 13:56:03 +0100

    calculator

--- 3.Objects/task.js
+++ 3.Objects/task.js
@@ -12,3 +12,18 @@ let a = new A;
 let b = new B
 
 console.log(a == b)
+
+
+function Calculator() {
+    this.read() = function () {
+        this.a = +prompt("a?", 0)
+        this.b = +prompt("b?", 0)
+    };
+    this.sum() = function() {
+        return this.a + this.b
+    };
+    this.mul() = function() {
+        return this.a * this.b
+    }
+}
+let calculator = new Calculator()
