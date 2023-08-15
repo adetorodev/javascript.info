49c7ea17949461f195a241d984cf85d8c7156c36
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-15 13:36:41 +0100

    Return from constructors

--- 3.Objects/constructor.js
+++ 3.Objects/constructor.js
@@ -1,10 +1,10 @@
-function User(name) {
-  this.name = name;
-  this.isAdmin = false;
-}
-let user = new User("Jack");
-console.log(user.name); // Jack
-console.log(user.isAdmin); // false
+// function User(name) {
+//   this.name = name;
+//   this.isAdmin = false;
+// }
+// let user = new User("Jack");
+// console.log(user.name); // Jack
+// console.log(user.isAdmin); // false
 
 /**
  When a function is executed as new User(...) , it does the following steps:
@@ -30,3 +30,25 @@ name: "Jack",
 isAdmin: false
 };
  */
+
+// Return from constructors
+
+// Usually, constructors do not have a return
+// statement. Their task is to write all necessary stuff
+// into this , and it automatically becomes the result.
+
+// If return is called with object, then it is returned instead of this .
+// If return is called with a primitive, it’s ignored.
+
+// function BigUser() {
+//   this.name = "John";
+//   return { name: "Godzilla" }; // <-- returns an object
+// }
+// console.log(new BigUser().name); // Godzilla, got that object ^^
+
+function SmallUser() {
+  this.name = "John";
+  return; // finishes the execution, returns this
+  // ...
+}
+console.log(new SmallUser().name); // John
