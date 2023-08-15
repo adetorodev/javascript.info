8b6f96f9d518000878818bbf01c7335b87b17518
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-15 14:14:42 +0100

    Accumulator

--- 3.Objects/task.js
+++ 3.Objects/task.js
@@ -27,3 +27,17 @@ function Calculator() {
     }
 }
 let calculator = new Calculator()
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+  
+    this.read = function() {
+      this.value += +prompt('How much to add?', 0);
+    };
+  
+  }
+  
+  let accumulator = new Accumulator(1);
+  accumulator.read();
+  accumulator.read();
+  alert(accumulator.value);
