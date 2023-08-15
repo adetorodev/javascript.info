adc9275abb540b41747dad926ef4bcf1b51a9500
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-15 13:13:42 +0100

    object to primitive conversion

--- object2primitive.js
+++ object2primitive.js
@@ -1,16 +1,16 @@
 // explicit conversion
-let num = Number(obj);
+// let num = Number(obj);
 // maths (except binary plus)
-let n = +obj; // unary plus
-let delta = date1 - date2;
+// let n = +obj; // unary plus
+// let delta = date1 - date2;
 // less/greater comparison
-let greater = user1 > user2;
+// let greater = user1 > user2;
 
 // obj == string/number/symbol
-if (user == 1) { ... };
+// if (user == 1) { ... };
 // The greater/less operator <> can work with both strings and numbers too.
 // binary plus
-let total = car1 + car2;
+// let total = car1 + car2;
 
 // To do the conversion, JavaScript tries to find and call three object methods:
 // 1. Call obj[Symbol.toPrimitive](hint) if the method exists,
