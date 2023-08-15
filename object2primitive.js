20bf56a74e07ed9bef843eae7421f71ac001b546
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-15 13:19:58 +0100

    Symbol.toPrimitive

--- object2primitive.js
+++ object2primitive.js
@@ -19,3 +19,33 @@
 // 3. Otherwise if hint is "number" or "default"
 // try obj.valueOf() and obj.toString() , whatever exists.
 
+// Symbol.toPrimitive
+
+/**
+ obj[Symbol.toPrimitive] = function(hint) {
+    return a primitive value
+     hint = one of "string", "number", "default"
+}
+**/
+
+let user = {
+  name: "John",
+  money: 1000,
+  [Symbol.toPrimitive](hint) {
+    console.log(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  },
+};
+
+// console.log(user)
+// console.log(+user)
+// console.log(user + 500)
+
+let userA = {
+  name: "John",
+  toString() {
+    return this.name;
+  },
+};
+console.log(userA); // toString -> John
+console.log(userA + 500); // toString -> John500
