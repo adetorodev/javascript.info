303da4ce7e339f3688488605a76cf60e77fb6cdc
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-16 15:49:13 +0100

    array add remove item

---
+++ 4.Data types/5.arrayMathod.js
@@ -0,0 +1,30 @@
+// let arr = ['I', 'go', 'home'];
+
+// delete arr[1]
+// console.log(arr[1])
+// console.log(arr)
+// console.log(arr.length)
+
+// console.log(arr.splice(1,1));
+// console.log(arr)
+// console.log(arr.length)
+
+// let arr = ["I", "study", "JavaScript", "right", "now"];
+// remove 3 first elements and replace them with another
+// arr.splice(0, 3, "Let's", "dance");
+// console.log(arr)
+
+// let arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+// arr.splice(2, 0, "complex", "language");
+
+// console.log( arr ); // "I", "study", "complex", "language", "JavaScript"
+
+// let arr = ["t", "e", "s", "t"];
+
+// console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+
+// console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
