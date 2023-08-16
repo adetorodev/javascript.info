502cb048675182da8b3480f1336cfb3d35789d0d
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-16 13:43:00 +0100

    Getting a substring

--- 4.Data types/3.strings.js
+++ 4.Data types/3.strings.js
@@ -79,11 +79,29 @@ alert( pos );
 */
 
 // includes, startsWith, endsWith
-console.log( "Widget with id".includes("Widget") ); // true
-console.log( "Hello".includes("Bye") ); // false
-console.log( "Midget".includes("id") ); // true
-console.log( "Midget".includes("id", 3) ); // false, from position 3 there is no "id"
-console.log( "Widget".startsWith("Wid") ); // true, "Widget" starts with "Wid"
-console.log( "Widget".endsWith("get") ); // true, "Widget" ends with "get"
+// console.log( "Widget with id".includes("Widget") ); // true
+// console.log( "Hello".includes("Bye") ); // false
+// console.log( "Midget".includes("id") ); // true
+// console.log( "Midget".includes("id", 3) ); // false, from position 3 there is no "id"
+// console.log( "Widget".startsWith("Wid") ); // true, "Widget" starts with "Wid"
+// console.log( "Widget".endsWith("get") ); // true, "Widget" ends with "get"
 
 // Getting a substring
+// str.slice(start [, end])
+let str = "stringify";
+console.log( str.slice(0, 5) ); // 'strin', the substring from 0 to 5 (not including 5)
+console.log( str.slice(0, 1) ); // 's', from 0 to 1, but not including 1, so only character at 0
+console.log( str.slice(2) ); // ringify, from the 2nd position till the end
+console.log( str.slice(-4, -1) ); // gif
+
+// str.substring(start [, end])
+// these are same for substring
+console.log( str.substring(2, 6) ); // "ring"
+console.log( str.substring(6, 2) ); // "ring"
+// ...but not for slice:
+console.log( str.slice(2, 6) ); // "ring" (the same)
+console.log( str.slice(6, 2) ); // "" (an empty string)
+
+// str.substr(start [, length])
+console.log( str.substr(2, 4) ); // ring, from the 2nd position get 4 characters
+console.log( str.substr(-4, 2) ); // gi, from the 4th position get 2 characters
