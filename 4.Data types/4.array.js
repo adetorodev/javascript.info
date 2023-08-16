4a72a1c24659be4c1ecca065c6b6a629409643e4
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-16 14:09:18 +0100

    Arrays

---
+++ 4.Data types/4.array.js
@@ -0,0 +1,26 @@
+// let arr = new Array
+// let arr = []
+
+// let fruits = ['Apple', 'Orange', 'Plum']
+// console.log(fruits)
+// console.log(fruits[1])
+
+// // replace element
+// fruits[2] = 'pear'
+// fruits[3] = 'lemon'
+// console.log(fruits)
+
+// // get length of the array
+// console.log(fruits.length)
+
+// let arr = ['apple', {name: 'John'}, true,  function(name) { console.log('Hello', name)}];
+// console.log(arr)
+// // get the object
+// console.log(arr[1].name)
+// // get the function
+// console.log(arr[3]('Kike'))
+
+let fruits = ['Apple', 'Orange', 'Plum']
+console.log(fruits[fruits.length-1])
+// same as fruits[fruits.length-1]
+console.log(fruits.at(-1))
