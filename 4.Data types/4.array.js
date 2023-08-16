ff9799dd0be9111cbab2dff95ada5c377dc7d5fa
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-16 14:17:41 +0100

    Methods pop/push, shift/unshift

--- 4.Data types/4.array.js
+++ 4.Data types/4.array.js
@@ -20,7 +20,33 @@
 // // get the function
 // console.log(arr[3]('Kike'))
 
-let fruits = ['Apple', 'Orange', 'Plum']
-console.log(fruits[fruits.length-1])
+// let fruits = ['Apple', 'Orange', 'Plum']
+// console.log(fruits[fruits.length-1])
 // same as fruits[fruits.length-1]
-console.log(fruits.at(-1))
+// console.log(fruits.at(-1))
+
+// Methods pop/push, shift/unshift
+
+// in queu
+// push appends an element to the end.
+// shift get an element from the beginning, advancing the queue, so that the 2nd element becomes the 1st.
+
+// in stack
+// push adds an element to the end.
+// pop takes an element from the end.
+
+// For stacks, the latest pushed item is received 
+// first, that’s also called LIFO 
+// (Last-In-First-Out) principle. For queues, we 
+// have FIFO (First-In-First-Out).
+// console.log(fruits.pop())
+// console.log(fruits)
+// fruits.push('pear')
+// console.log(fruits)
+// console.log(fruits.shift())
+// console.log(fruits)
+// fruits.unshift('apple')
+// console.log(fruits)
+
+
+// Internals
