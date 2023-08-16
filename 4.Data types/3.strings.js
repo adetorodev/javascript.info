43e6c2a55ecc8f7553d3abe3130d5b93ccd43c05
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-16 13:08:07 +0100

    Special characters

--- 4.Data types/3.strings.js
+++ 4.Data types/3.strings.js
@@ -1,11 +1,26 @@
-function sum(a, b) {
-  return a + b;
-}
-console.log(`1 + 2 = ${sum(1, 2)}.`); // 1 + 2 = 3.
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(`1 + 2 = ${sum(1, 2)}.`); // 1 + 2 = 3.
 
-let guestList = `Guests:
-* John
-* Pete
-* Mary
-`;
-console.log(guestList);
+// let guestList = `Guests:
+// * John
+// * Pete
+// * Mary
+// `;
+// console.log(guestList);
+
+
+// Special characters
+// let guestList = "Guests:\n * John\n * Pete\n * Mary"
+// console.log(guestList);
+
+// let str1 = "Hello\nWorld"; // two lines using a "newline symbol"
+// two lines using a normal newline and backticks
+// let str2 = `Hello
+// World`;
+// console.log(str1 == str2); // true
+
+console.log( "\u00A9" ); // ©
+console.log( "\u{20331}" ); // 佫, a rare Chinese hieroglyph (long unicode)
+console.log( "\u{1F60D}" ); // 😍, a smiling face symbol (another long unicode)
