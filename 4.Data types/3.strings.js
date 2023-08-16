5baa9a390b0d98364507745ce01838c2f628096c
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-16 13:44:56 +0100

    string

--- 4.Data types/3.strings.js
+++ 4.Data types/3.strings.js
@@ -88,20 +88,22 @@ alert( pos );
 
 // Getting a substring
 // str.slice(start [, end])
-let str = "stringify";
-console.log( str.slice(0, 5) ); // 'strin', the substring from 0 to 5 (not including 5)
-console.log( str.slice(0, 1) ); // 's', from 0 to 1, but not including 1, so only character at 0
-console.log( str.slice(2) ); // ringify, from the 2nd position till the end
-console.log( str.slice(-4, -1) ); // gif
+// let str = "stringify";
+// console.log( str.slice(0, 5) ); // 'strin', the substring from 0 to 5 (not including 5)
+// console.log( str.slice(0, 1) ); // 's', from 0 to 1, but not including 1, so only character at 0
+// console.log( str.slice(2) ); // ringify, from the 2nd position till the end
+// console.log( str.slice(-4, -1) ); // gif
 
 // str.substring(start [, end])
 // these are same for substring
-console.log( str.substring(2, 6) ); // "ring"
-console.log( str.substring(6, 2) ); // "ring"
+// console.log( str.substring(2, 6) ); // "ring"
+// console.log( str.substring(6, 2) ); // "ring"
 // ...but not for slice:
-console.log( str.slice(2, 6) ); // "ring" (the same)
-console.log( str.slice(6, 2) ); // "" (an empty string)
+// console.log( str.slice(2, 6) ); // "ring" (the same)
+// console.log( str.slice(6, 2) ); // "" (an empty string)
 
 // str.substr(start [, length])
-console.log( str.substr(2, 4) ); // ring, from the 2nd position get 4 characters
-console.log( str.substr(-4, 2) ); // gi, from the 4th position get 2 characters
+// console.log( str.substr(2, 4) ); // ring, from the 2nd position get 4 characters
+// console.log( str.substr(-4, 2) ); // gi, from the 4th position get 2 characters
+
+
