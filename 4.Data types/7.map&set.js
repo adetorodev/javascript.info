2f39b3a3fceb37faf63c9c88401fd3cabd6413ef
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-18 22:56:44 +0100

    set

--- 4.Data types/7.map&set.js
+++ 4.Data types/7.map&set.js
@@ -64,3 +64,36 @@ map.size – returns the current element count.
 //   for (let entry of recipeMap) { // the same as of recipeMap.entries()
 //     console.log(entry); // cucumber,500 (and so on)
 //   }
+
+// A Set is a special type collection – “set of values” 
+// (without keys), where each value may occur only once.
+
+/*
+new Set([iterable]) – creates the set, and if an iterable object is provided (usually an array), copies values from it into the set.
+set.add(value) – adds a value, returns the set itself.
+set.delete(value) – removes the value, returns true if value existed at the moment of the call, otherwise false.
+set.has(value) – returns true if the value exists in the set, otherwise false.
+set.clear() – removes everything from the set.
+set.size – is the elements count.
+*/
+
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// visits, some users come multiple times
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+// set keeps only unique values
+console.log( set.size ); // 3
+
+for (let user of set) {
+  console.log(user.name); // John (then Pete and Mary)
+}
+console.log(set)
