8c79ebf821d4d64249584fb9201d500dd704afd1
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-18 23:44:39 +0100

    Filter anagrams

--- 4.Data types/7.map&set.js
+++ 4.Data types/7.map&set.js
@@ -146,3 +146,18 @@ function aclean(arr) {
 let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
 console.log(aclean(arr));
+
+function acleanRe(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let sorted = arr[i].toLowerCase().split("").sort().join("");
+    obj[sorted] = arr[i];
+  }
+
+  return Object.values(obj);
+}
+
+let arrRe = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+console.log(aclean(arrRe));
