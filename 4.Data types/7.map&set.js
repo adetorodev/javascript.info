26f72533c7ace1b8303068eb6c5fe2e9e03d00b4
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-18 22:58:01 +0100

    looping

--- 4.Data types/7.map&set.js
+++ 4.Data types/7.map&set.js
@@ -77,23 +77,34 @@ set.clear() – removes everything from the set.
 set.size – is the elements count.
 */
 
-let set = new Set();
+// let set = new Set();
 
-let john = { name: "John" };
-let pete = { name: "Pete" };
-let mary = { name: "Mary" };
+// let john = { name: "John" };
+// let pete = { name: "Pete" };
+// let mary = { name: "Mary" };
 
-// visits, some users come multiple times
-set.add(john);
-set.add(pete);
-set.add(mary);
-set.add(john);
-set.add(mary);
+// // visits, some users come multiple times
+// set.add(john);
+// set.add(pete);
+// set.add(mary);
+// set.add(john);
+// set.add(mary);
 
 // set keeps only unique values
-console.log( set.size ); // 3
+// console.log( set.size ); // 3
 
-for (let user of set) {
-  console.log(user.name); // John (then Pete and Mary)
-}
-console.log(set)
+// for (let user of set) {
+//   console.log(user.name); // John (then Pete and Mary)
+// }
+// console.log(set)
+
+// Looping
+
+let set = new Set(["oranges", "apples", "bananas"]);
+
+for (let value of set) alert(value);
+
+// the same with forEach:
+set.forEach((value, valueAgain, set) => {
+  alert(value);
+});
