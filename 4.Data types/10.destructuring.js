748a09273e97c295c929514b929a5d8feb4794eb
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-21 16:42:48 -0500

    console.log

--- 4.Data types/10.destructuring.js
+++ 4.Data types/10.destructuring.js
@@ -1,4 +1,4 @@
-// 
+//
 /* 
 Destructuring assignment is a special syntax that allows us to “unpack” arrays or objects into a bunch of variables, as sometimes that’s more convenient.
 
@@ -6,7 +6,7 @@ Destructuring also works great with complex functions that have a lot of paramet
 */
 
 // we have an array with the name and surname
-let arr = ["John", "Smith"]
+let arr = ["John", "Smith"];
 
 // destructuring assignment
 // sets firstName = arr[0]
@@ -20,7 +20,52 @@ let arr = ["John", "Smith"]
 // console.log(firstName); // John
 // console.log(surname);  // Smith
 // second element is not needed
-let [firstName, , title] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
+// let [firstName, , title] = [
+//   "Julius",
+//   "Caesar",
+//   "Consul",
+//   "of the Roman Republic",
+// ];
 
-console.log( title ); // Consul
-console.log( firstName );
+// console.log(title); // Consul
+// console.log(firstName);
+
+// let options = {
+//   title: "Menu",
+//   width: 100,
+//   height: 200,
+// };
+
+// let { title, width, height } = options;
+
+// console.log(title); // Menu
+// console.log(width); // 100
+// console.log(height); // 200
+
+// Nested destructuring
+
+let options = {
+  size: {
+    width: 100,
+    height: 200,
+  },
+  items: ["Cake", "Donut"],
+  extra: true,
+};
+
+// destructuring assignment split in multiple lines for clarity
+let {
+  size: {
+    // put size here
+    width,
+    height,
+  },
+  items: [item1, item2], // assign items here
+  title = "Menu", // not present in the object (default value is used)
+} = options;
+
+console.log(title); // Menu
+console.log(width); // 100
+console.log(height); // 200
+console.log(item1); // Cake
+console.log(item2); // Donut
