24ee0afc996b29a6eca34977d053a8cd94d31923
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-22 06:25:23 -0500

    Spread operator

--- 5.Advance Function/2.restParameter.js
+++ 5.Advance Function/2.restParameter.js
@@ -8,3 +8,13 @@ function sumAll(...args) {
 console.log( sumAll(1) ); // 1
 console.log( sumAll(1, 2) ); // 3
 console.log( sumAll(1, 2, 3) ); // 6
+
+// Spread operator
+
+let arr = [3, 5, 1];
+console.log( Math.max(arr) ); // NaN
+console.log( Math.max(...arr) );
+
+let arr1 = [1, -2, 3, 4];
+let arr2 = [8, 3, -8, 1];
+console.log( Math.max(...arr1, ...arr2) );
