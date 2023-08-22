3191e95a2114990789c282264c0c83da36ad9c3c
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-22 05:38:59 -0500

    Recursive

---
+++ 5.Advance Function/1.recursionStack.js
@@ -0,0 +1,67 @@
+// pow(2,2,)
+
+// function pow(x,n){
+//     let result = 1
+
+//     // multiply result by x n times in the loop
+//     for(let i=0; i < n; i++){
+//         result *= x
+//     }
+//     return result
+// }
+
+// console.log(pow(2,2,))
+// console.log(pow(2, 4))
+
+// Recursive thinking
+
+function pow(x, n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+console.log(pow(2, 2));
+console.log(pow(2, 4));
+
+// x * pow(x, n - 1) ==> xn = x * xn-1 .
+
+// using conditional operator
+function pow(x, n) {
+  return n == 1 ? x : x * pow(x, n - 1);
+}
+
+let company = {
+  // the same object, compressed for brevity
+  sales: [
+    { name: "John", salary: 1000 },
+    { name: "Alice", salary: 600 },
+  ],
+  development: {
+    sites: [
+      { name: "Peter", salary: 2000 },
+      { name: "Alex", salary: 1800 },
+    ],
+    internals: [{ name: "Jack", salary: 1300 }],
+  },
+};
+
+// The function to do the job
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    // case (1)
+    return department.reduce((prev, current) => prev + current.salary, 0); // sum the array
+  } else {
+    // case (2)
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep); // recursively call for subdepartments, sum the results
+    }
+    return sum;
+  }
+}
+console.log(sumSalaries(company));
+
+//
