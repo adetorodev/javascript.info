d085c0ee572e9ee4f0993d71483a927d0719d23c
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-22 06:39:21 -0500

    Function Expression

---
+++ 5.Advance Function/4.functionObject.js
@@ -0,0 +1,15 @@
+// function sayHi() {
+//   console.log("Hi");
+// }
+// console.log(sayHi.name);
+
+// Function Expression
+let sayHi = function () {
+  console.log("Hi");
+};
+console.log(sayHi.name);
+
+// function f(sayHi = function () {}) {
+//     console.log(sayHi.name); // sayHi (works!)
+// }
+// f();
