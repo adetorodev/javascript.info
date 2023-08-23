07663181e39928acbd652fbaf63a22e53c9cbaac
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-23 16:30:20 -0500

    Overriding a method

--- 7.Class/2.classInheritance.js
+++ 7.Class/2.classInheritance.js
@@ -28,12 +28,31 @@ let animal = new Animal("My Animal");
 // console.log(rabbit)
 
 // Inherit from Animal by specifying "extends Animal"
+// class Rabbit extends Animal {
+//   hide() {
+//     console.log(`${this.name} hides!`);
+//   }
+// }
+// let rabbit = new Rabbit("White Rabbit");
+
+// rabbit.run(5); // White Rabbit runs with speed 5.
+// rabbit.hide(); // White Rabbit hides!
+
+// Overriding a method
+
 class Rabbit extends Animal {
   hide() {
     console.log(`${this.name} hides!`);
   }
+  stop() {
+    super.stop(); // call parent stop
+    this.hide(); // and then hide
+  }
 }
 let rabbit = new Rabbit("White Rabbit");
-
 rabbit.run(5); // White Rabbit runs with speed 5.
-rabbit.hide(); // White Rabbit hides!
+rabbit.stop(); // White Rabbit stopped. White rabbit hides!
+
+// Classes provide "super" keyword for that.
+// ●super.method(...) to call a parent method.
+// ●super(...) to call a parent constructor (inside our constructor only).
