96158753703f148e9173f687284250ed7e50f6d9
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-23 10:51:04 -0500

    Tasks

--- 7.Class/01.classBasic.js
+++ 7.Class/01.classBasic.js
@@ -88,17 +88,56 @@
 
 // dynamic class
 
-function makeClass(phrase) {
-  // declare a class and return it
-  return class {
-    sayHi() {
-      console.log(phrase);
-    }
-  };
-}
+// function makeClass(phrase) {
+//   // declare a class and return it
+//   return class {
+//     sayHi() {
+//       console.log(phrase);
+//     }
+//   };
+// }
 // Create a new class
-let Userc = makeClass("Hello");
-new Userc().sayHi(); // Hello
-
+// let Userc = makeClass("Hello");
+// new Userc().sayHi(); // Hello
 
 // Getters/setters, other shorthands
+// class User {
+//   constructor(name) {
+//     // invokes the setter
+//     this.name = name;
+//   }
+//   get name() {
+//     return this._name;
+//   }
+//   set name(value) {
+//     if (value.length < 4) {
+//       console.log("Name is too short.");
+//       return;
+//     }
+//     this._name = value;
+//   }
+// }
+// let user = new User("John");
+// console.log(user.name); // John
+// user = new User(""); // Name too short.
+
+// Object.defineProperties(User.prototype, {
+//   name: {
+//     get() {
+//       return this._name;
+//     },
+//     set(name) {
+//       // ...
+//     },
+//   },
+// });
+
+function f() {
+  return "sayHi";
+}
+class User {
+  [f()]() {
+    console.log("Hello");
+  }
+}
+new User().sayHi();
