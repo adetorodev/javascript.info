347e3c2193f16de65ca2488dd88cf15ce799bf88
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-24 15:55:08 -0500

    callback

--- 9.asyncJavascript/introduction.js
+++ 9.asyncJavascript/introduction.js
@@ -1,8 +1,15 @@
-function loadScript(src) {
-  let script = document.createElement("script");
-  script.src = src;
-  document.head.append(script);
-}
+// function loadScript(src) {
+//   let script = document.createElement("script");
+//   script.src = src;
+//   document.head.append(script);
+// }
+
+// function loadScript(src, callback) {
+//   let script = document.createElement("script");
+//   script.src = src;
+//   script.onload = () => callback(script);
+//   document.head.append(script);
+// }
 
 function loadScript(src, callback) {
   let script = document.createElement("script");
@@ -10,3 +17,10 @@ function loadScript(src, callback) {
   script.onload = () => callback(script);
   document.head.append(script);
 }
+loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js",
+  (script) => {
+    console.log(`Cool, the ${script.src} is loaded`);
+    console.log(_); // function declared in the loaded script
+  }
+);
