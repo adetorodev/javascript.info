d9cb30a9e45adc59603339c580af90642c8bb3d6
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-24 15:57:49 -0500

    Handling errors

--- 9.asyncJavascript/introduction.js
+++ 9.asyncJavascript/introduction.js
@@ -42,3 +42,20 @@ loadScript("/my/script.js", function (script) {
     });
   });
 });
+
+// Handling errors
+function loadScript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error for ${src}`));
+  document.head.append(script);
+}
+
+loadScript("/my/script.js", function (error, script) {
+  if (error) {
+    // handle error
+  } else {
+    // script loaded successfully
+  }
+});
