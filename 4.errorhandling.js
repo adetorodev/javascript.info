3d14747b50c0f1a31cdf72d0f5e2855c3a3c00e2
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-28 18:20:39 -0500

    Implicit try…catch

--- 4.errorhandling.js
+++ 4.errorhandling.js
@@ -25,3 +25,53 @@ fetch("/article/promise-chaining/user.json")
       })
   )
   .catch((error) => console.log(error.message));
+
+// Implicit try…catch
+
+new Promise((resolve, reject) => {
+  throw new Error("Whoops!");
+}).catch(alert); // Error: Whoops!
+
+//    …Works exactly the same as this:
+new Promise((resolve, reject) => {
+  reject(new Error("Whoops!"));
+}).catch(alert); // Error: Whoops!
+
+new Promise((resolve, reject) => {
+  resolve("ok");
+})
+  .then((result) => {
+    throw new Error("Whoops!"); // rejects the promise
+  })
+  .catch(alert); // Error: Whoops!
+
+// the execution: catch -> then
+new Promise((resolve, reject) => {
+  throw new Error("Whoops!");
+})
+  .catch(function (error) {
+    alert("The error is handled, continue normally");
+  })
+  .then(() => alert("Next successful handler runs"));
+
+// the execution: catch -> catch -> then
+new Promise((resolve, reject) => {
+  throw new Error("Whoops!");
+})
+  .catch(function (error) {
+    // (*)
+    if (error instanceof URIError) {
+      // handle it
+    } else {
+      alert("Can't handle such error");
+      throw error; // throwing this or another error jumps to the next catch
+    }
+  })
+  .then(function () {
+    /* never runs here */
+  })
+  .catch((error) => {
+    // (**)
+    alert(`The unknown error has occurred: ${error}`);
+    // don't return anything => execution goes the normal way
+  });
