781c265083fb4a6cf0ec167b575e1bb4ab7a2e77
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-28 18:00:11 -0500

    chaining promises

---
+++ 9.asyncJavascript/33.promiseChaining.js
@@ -0,0 +1,41 @@
+new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(1), 1000); // (*)
+})
+  .then(function (result) {
+    // (**)
+    console.log(result); // 1
+    return result * 2;
+  })
+  .then(function (result) {
+    // (***)
+    console.log(result); // 2
+    return result * 2;
+  })
+  .then(function (result) {
+    console.log(result); // 4
+    return result * 2;
+  });
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(1), 1000);
+}).then(function (result) {
+  console.log(result);
+  return result * 2; // <-- (1)
+}); // <-- (2)
+// .then…
+
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(1), 1000);
+});
+promise.then(function (result) {
+  console.log(result); // 1
+  return result * 2;
+});
+promise.then(function (result) {
+  console.log(result); // 1
+  return result * 2;
+});
+promise.then(function (result) {
+  console.log(result); // 1
+  return result * 2;
+});
