28fc3d7223e29ac38e9c792d630f97058e444f17
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-29 16:27:53 -0500

    Promise.resolve

---
+++ 9.asyncJavascript/5.promiseAPI.js
@@ -0,0 +1,17 @@
+// Promise.resolve
+// let promise = Promise.resolve(value);
+// Same as:
+// let promise = new Promise(resolve => resolve(value));
+
+function loadCached(url) {
+  let cache = loadCached.cache || (loadCached.cache = new Map());
+  if (cache.has(url)) {
+    return Promise.resolve(cache.get(url)); 
+  }
+  return fetch(url)
+    .then((response) => response.text())
+    .then((text) => {
+      cache.set(url, text);
+      return text;
+    });
+}
